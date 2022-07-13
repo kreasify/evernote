7ddaeff25889cda69ad2d05e66c94a5a90f4385d
Akhlis <kreasify@gmail.com>
2022-07-13 20:18:37 +0700

    post

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,16 +2,16 @@ import './../css/style.css'
 // import './../js/isotope.pkgd.min.js'
 // import './../js/splide.min.js'
 import './../js/alpinejs@3.9.1.min.js'
-import menusData from './../data/menus.json'
+// import menusData from './../data/menus.json'
 // import posts from './../data/templates.json'
 
-document.addEventListener('alpine:init', () => {
-  Alpine.data('header', () => ({
-    menus: menusData.menus,
-    sideNav: false,
-    atTop: true
-  }))
-})
+// document.addEventListener('alpine:init', () => {
+//   Alpine.data('header', () => ({
+//     menus: menusData.menus,
+//     sideNav: false,
+//     atTop: true
+//   }))
+// })
 
 // document.addEventListener('alpine:init', () => {
 //   Alpine.data('posts', () => ({
