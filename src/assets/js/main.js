ad92bd9cc65e7503a0847aa6f582663b66ca08b6
Akhlis <kreasify@gmail.com>
2022-07-17 14:44:15 +0700

    fix menu error

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,20 +1,2 @@
-import './../css/style.css'
-// import './../js/isotope.pkgd.min.js'
-// import './../js/splide.min.js'
 import './../js/alpinejs@3.9.1.min.js'
-// import menusData from './../data/menus.json'
-// import posts from './../data/templates.json'
-
-// document.addEventListener('alpine:init', () => {
-//   Alpine.data('header', () => ({
-//     menus: menusData.menus,
-//     sideNav: false,
-//     atTop: true
-//   }))
-// })
-
-// document.addEventListener('alpine:init', () => {
-//   Alpine.data('posts', () => ({
-//     posts: posts.templates,
-//   }))
-// })
+import './../css/style.css'
